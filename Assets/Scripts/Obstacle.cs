0dbac4e50cd877eeb60fc67244e7c0f76ec9e764
Ozgur Cetin <ozgur.cetin@pego.co.uk>
2025-12-07 00:48:29 +0000

    comments

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -15,25 +15,21 @@ public class Obstacle : MonoBehaviour
 
     private void Start()
     {
-        // Random size
         var size = Random.Range(minSize, maxSize);
         transform.localScale = new Vector3(size, size, 1);
 
         _rb = GetComponent<Rigidbody2D>();
 
-        // Random direction + impulse
         Vector2 direction = Random.insideUnitCircle.normalized;
         var speed = Random.Range(minSpeed, maxSpeed);
 
         _rb.AddForce(direction * speed, ForceMode2D.Impulse);
 
-        // Spin
         _rb.AddTorque(Random.Range(-maxSpinSpeed, maxSpinSpeed), ForceMode2D.Impulse);
     }
 
     private void FixedUpdate()
     {
-        // Prevent stopping
         const float minVelocity = 3f;
         if (_rb.linearVelocity.magnitude < minVelocity)
         {
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,27 +7,28 @@ public class PlayerController : MonoBehaviour
 {
     private Camera _camera;
     private Rigidbody2D _rb;
-    
+
     public float maxSpeed = 5f;
     public float thrustForce = 7f;
     public float rotationSpeed = 15f;
-    
+
     private Vector3 _mouseWorldPos;
     private bool _isThrusting;
-    
+
     public UIDocument uiDocument;
-    
+
     private float _elapsedTime;
-    
+
     private Label _scoreText;
     private Button _restartButton;
     public GameObject explosionEffect;
+    // public ParticleSystem boosterParticles;
 
     private void Start()
     {
         _camera = Camera.main;
         _rb = GetComponent<Rigidbody2D>();
-        
+
         // UI setup 
         var root = uiDocument.rootVisualElement;
         _scoreText = root.Q<Label>("ScoreLabel");
@@ -35,34 +36,44 @@ public class PlayerController : MonoBehaviour
         _restartButton.style.display = DisplayStyle.None;
         _restartButton.clicked += ReloadScene;
     }
+
     private void Update()
     {
-        // Score update
         _elapsedTime += Time.deltaTime;
         _scoreText.text = ((int)((_elapsedTime + Time.deltaTime) * 10)).ToString();
-        
+
         if (_restartButton.style.display == DisplayStyle.Flex &&
             Keyboard.current.spaceKey.wasPressedThisFrame)
         {
             ReloadScene();
         }
-        
-        // handle input, get mouse world position
+
         _isThrusting = Mouse.current.leftButton.isPressed;
 
+        // if (_isThrusting)
+        // {
+        //     if (!boosterParticles.isPlaying)
+        //         boosterParticles.Play();
+        // }
+        // else
+        // {
+        //     if (boosterParticles.isPlaying)
+        //         boosterParticles.Stop();
+        // }
+
         if (!_isThrusting) return;
-        
+
         Vector3 mousePos = Mouse.current.position.value;
         mousePos.z = 0f;
         _mouseWorldPos = _camera.ScreenToWorldPoint(mousePos);
     }
-    
+
     private void FixedUpdate()
     {
         if (_isThrusting)
         {
             Vector2 diff = _mouseWorldPos - transform.position;
-            
+
             if (diff.magnitude < 2f)
                 return;
 
@@ -71,7 +82,6 @@ public class PlayerController : MonoBehaviour
             var smoothed = Mathf.LerpAngle(_rb.rotation, targetAngle, rotationSpeed * Time.fixedDeltaTime);
             _rb.MoveRotation(smoothed);
 
-            // Apply thrust
             _rb.AddForce(direction * thrustForce);
         }
 
@@ -85,9 +95,9 @@ public class PlayerController : MonoBehaviour
         Instantiate(explosionEffect, transform.position, Quaternion.identity);
 
         Destroy(gameObject);
-        
+
         _restartButton.style.display = DisplayStyle.Flex;
-        
+
         _scoreText.text = $"smashed: {_scoreText.text}";
     }
 
